0a7f7a233ffa53483d6d04c0830bf9c5afbb9f96
MatheusCampus <83256336+MatheusCampus@users.noreply.github.com>
2021-11-11 19:00:07 -0400

    Inicio da Implementação do codigo do firebase no codigo e melhoria nos nomes do layouts

--- APProfisso/app/src/main/java/com/example/approfisso/classes/empregos.java
+++ APProfisso/app/src/main/java/com/example/approfisso/classes/empregos.java
@@ -1,76 +1,91 @@
 package com.example.approfisso.classes;
 
+
 import java.io.Serializable;
 
 public class empregos implements Serializable {
 
-    private String id;
-    private String estado;
-    private String cidade;
-    private String periodo;
-    private String area_da_profissao;
-    int salario;
-    private String email;
+    private Integer id;
+    private String Estado;
+    private String Cidade;
+    private String Periodo;
+    private String Area_da_profissao;
+    private String Salario;
+    private String Email;
 
-    public empregos(String id) {this.id = id;}
-
-    public  empregos(){
+    public empregos(Integer id) {
+        this.id = id;
     }
 
-
-    public String getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
     public String getEstado() {
-        return estado;
+        return Estado;
     }
 
     public void setEstado(String estado) {
-        this.estado = estado;
+        Estado = estado;
     }
 
     public String getCidade() {
-        return cidade;
+        return Cidade;
     }
 
     public void setCidade(String cidade) {
-        this.cidade = cidade;
+        Cidade = cidade;
     }
 
     public String getPeriodo() {
-        return periodo;
+        return Periodo;
     }
 
     public void setPeriodo(String periodo) {
-        this.periodo = periodo;
+        Periodo = periodo;
     }
 
     public String getArea_da_profissao() {
-        return area_da_profissao;
+        return Area_da_profissao;
     }
 
     public void setArea_da_profissao(String area_da_profissao) {
-        this.area_da_profissao = area_da_profissao;
+        Area_da_profissao = area_da_profissao;
     }
 
-    public int getSalario() {
-        return salario;
+    public String getSalario() {
+        return Salario;
     }
 
-    public void setSalario(int salario) {
-        this.salario = salario;
+    public void setSalario(String salario) {
+        Salario = salario;
     }
 
     public String getEmail() {
-        return email;
+        return Email;
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        Email = email;
     }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + Area_da_profissao + '\'' +
+                ", salario='" + Salario + '\'' +
+                '}';
+    }
+
+
+
 }
+
